87f8894cdf6a3fc8b191868a6c8bea05ca544515
namhk <namhkse@gmail.com>
2022-11-10 01:04:09 +0700

    kitty config, zsh prompt

--- lvim/config.lua
+++ lvim/config.lua
@@ -1,36 +1,27 @@
---[[
-lvim is the global options object
-
-Linters should be
-filled in as strings with either
-a global executable or a path to
-an executable
-]]
--- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
-
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save = true
-lvim.colorscheme = "gruvbox"
--- to disable icons and use a minimalist setup, uncomment the following
--- lvim.use_icons = false
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
+
 -- add your own keymapping
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 vim.keymap.set("i", "<C-s>", "<esc>:w<cr>i")
-vim.keymap.set("n", "te", ":tabedit<space>")
+vim.keymap.set("i", "jj", "<esc>")
+
 vim.keymap.set("", "sh", "<C-w>h")
 vim.keymap.set("", "sl", "<C-w>l")
+vim.keymap.set("n", "te", ":tabedit<space>")
 vim.keymap.set("n", "sv", ":vsplit<space>")
 vim.keymap.set("n", "<C-q>", ":q<cr>")
 vim.keymap.set("n", "<C-b>", ":NvimTreeToggle<cr>")
--- lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
--- lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+lvim.keys.normal_mode["<A-l>"] = ":BufferLineCycleNext<CR>"
+lvim.keys.normal_mode["<A-h>"] = ":BufferLineCyclePrev<CR>"
+
 -- unmap a default keymapping
 -- vim.keymap.del("n", "<C-Up>")
 -- override a default keymapping
@@ -55,7 +46,12 @@ vim.keymap.set("n", "<C-b>", ":NvimTreeToggle<cr>")
 -- }
 
 -- Use which-key to add extra bindings with the leader-key prefix
--- lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+lvim.builtin.which_key.mappings["m"] = {
+    "<cmd>w | make<CR>", "Makefile"
+}
+
+lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
+
 -- lvim.builtin.which_key.mappings["t"] = {
 --   name = "+Trouble",
 --   r = { "<cmd>Trouble lsp_references<cr>", "References" },
@@ -70,26 +66,25 @@ vim.keymap.set("n", "<C-b>", ":NvimTreeToggle<cr>")
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
 lvim.builtin.bufferline.options.indicator_icon = nil
 lvim.builtin.bufferline.options.indicator = { styles = "icon", icon = "| " }
--- if you don't want all the parsers change this to a table of the ones you want
+
 lvim.builtin.treesitter.ensure_installed = {
-  "bash",
-  "c",
-  "javascript",
-  "json",
-  "lua",
-  "python",
-  "typescript",
-  "tsx",
-  "css",
-  "rust",
-  "java",
-  "yaml",
+    "bash",
+    "c",
+    "javascript",
+    "json",
+    "lua",
+    "python",
+    "typescript",
+    "tsx",
+    "css",
+    "rust",
+    "java",
+    "yaml",
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
@@ -172,24 +167,10 @@ lvim.builtin.treesitter.highlight.enabled = true
 
 -- Additional Plugins
 lvim.plugins = {
-  { "folke/tokyonight.nvim" },
-  { "morhetz/gruvbox" },
-  {
-    "folke/trouble.nvim",
-    cmd = "TroubleToggle",
-  },
+    { "morhetz/gruvbox" },
+    { "ryanoasis/vim-devicons" },
+    {
+        "folke/trouble.nvim",
+        cmd = "TroubleToggle",
+    },
 }
-
--- Autocommands (https://neovim.io/doc/user/autocmd.html)
--- vim.api.nvim_create_autocmd("BufEnter", {
---   pattern = { "*.json", "*.jsonc" },
---   -- enable wrap mode for json files only
---   command = "setlocal wrap",
--- })
--- vim.api.nvim_create_autocmd("FileType", {
---   pattern = "zsh",
---   callback = function()
---     -- let treesitter use bash highlight for zsh files as well
---     require("nvim-treesitter.highlight").attach(0, "bash")
---   end,
--- })
